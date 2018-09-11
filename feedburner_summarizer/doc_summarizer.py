5de9f656724528dac2be7d142cbc645d54c373e8
Michael <Michaelliv@users.noreply.github.com>
2018-09-11 08:05:03 +0300

    cleanup and requierments update

--- feedburner_summarizer/doc_summarizer.py
+++ feedburner_summarizer/doc_summarizer.py
@@ -1,23 +1,21 @@
+import asyncio
+from collections import ChainMap
+from collections import Counter
 from concurrent.futures import ThreadPoolExecutor
 from typing import List
 
-from collections import Counter
-from collections import ChainMap
-
-import asyncio
-import spacy
-
 import numpy as np
+import spacy
 import wikipedia
 
 from feedburner_summarizer.definitions import EN_MOST_COMMON_FILE
 from feedburner_summarizer.rss_handler import RSSData
 
 
-class DocSummarizer():
-
+class DocSummarizer:
     def __init__(self):
         self.executor = ThreadPoolExecutor(max_workers=3)
+        self.loop = asyncio.get_event_loop()
         self.en_most_common = set(self.load_en_most_common_file())
         self.nlp = spacy.load('en_core_web_sm')
 
@@ -29,22 +27,24 @@ class DocSummarizer():
 
     def valid_token(self, token):
         return (
-            token.is_alpha and                   # token is alpha numeric
-            token.pos_ in ["PROPN", "NOUN"] and  # token is either noun or proper noun
-            not token.is_punct and               # token is not punctuation
-            not token.is_stop and                # token is not is not a stop word
-            not token in self.en_most_common and # token is not one of english's most used words
+            token.is_alpha and                    # token is alpha numeric
+            token.pos_ in ["PROPN", "NOUN"] and   # token is either noun or proper noun
+            not token.is_punct and                # token is not punctuation
+            not token.is_stop and                 # token is not is not a stop word
+            token not in self.en_most_common and  # token is not one of english's most used words
             len(token) > 2
         )
 
-    def load_en_most_common_file(self, file_path: str = EN_MOST_COMMON_FILE):
+    @staticmethod
+    def load_en_most_common_file(file_path: str = EN_MOST_COMMON_FILE):
         with open(file_path, "r") as f:
             return [word for word in f.read().split("\n")]
 
     def cleanup_en_most_common_words(self, text_list: List[str]) -> List[str]:
         return [word for word in text_list if word not in self.en_most_common]
 
-    def query_wikipedia(self, text):
+    @staticmethod
+    def query_wikipedia(text):
         return {text: [w.lower() for w in wikipedia.search(text)]}
 
     async def summarize(self, rss_data: RSSData):
@@ -60,12 +60,12 @@ class DocSummarizer():
         quartile_1, quartile_3 = np.percentile(values, [25, 75])
         iqr = quartile_3 - quartile_1
         upper_bound = quartile_3 + (iqr * 1.5)
-        filtered_words = [k for k,v in word_count.items() if v > upper_bound]
+        filtered_words = [k for k, v in word_count.items() if v > upper_bound]
 
         # Phase #3 - Validation thorough Wikipedia
-        loop = asyncio.get_event_loop()
-        futures = [loop.run_in_executor(self.executor, self.query_wikipedia, word) for word in filtered_words]
+        futures = [self.loop.run_in_executor(self.executor, self.query_wikipedia, word) for word in filtered_words]
         await asyncio.wait(futures)
+
         wiki_results = dict(ChainMap(*[f.result() for f in futures]))
         wikipedia_validated = [word for word in filtered_words if word in wiki_results.get(word, False)]
 
