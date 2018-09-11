532d74a8f915e2a85604d8a053879455de6742fa
Michael <Michaelliv@users.noreply.github.com>
2018-09-11 21:24:44 +0300

    minor change

--- feedburner_summarizer/rss_handler.py
+++ feedburner_summarizer/rss_handler.py
@@ -12,7 +12,7 @@ class EntriesNotFoundError(Exception):
     pass
 
 
-class RSSData():
+class RSSData:
     def __init__(self, url: str, title: str, content: str):
         self.url = url
         self.title = title
